1d6bbba0a8d110518743d4663ea655ac2e0279b2
securethebags <anthonyjim96@gmail.com>
2020-06-01 21:37:06 -0400

    Version 1.0 on Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const path = require("path");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use("/api/v1/calories", require("./routes/calorieEvents"));
 app.use("/api/v1/users", require("./routes/users"));
 app.use("/api/v1/auth", require("./routes/auth"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
